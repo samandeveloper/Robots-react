1593ba79777ec646ad4c9dac1375ce6ec885452f
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-16 13:23:24 -0700

    Updates

---
+++ precache-manifest.81dd9b978159985fae3db63b2f8f33ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb243f6cde197f6faeca457db8e09f6",
+    "url": "/robots-react/index.html"
+  },
+  {
+    "revision": "759e2e68a6f77f65c36f",
+    "url": "/robots-react/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "8a98b3c5b2fa5a822ed0",
+    "url": "/robots-react/static/css/main.bc5f3d73.chunk.css"
+  },
+  {
+    "revision": "759e2e68a6f77f65c36f",
+    "url": "/robots-react/static/js/2.31b87c09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robots-react/static/js/2.31b87c09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a98b3c5b2fa5a822ed0",
+    "url": "/robots-react/static/js/main.9e05b65d.chunk.js"
+  },
+  {
+    "revision": "de63d3e815469bc5a134",
+    "url": "/robots-react/static/js/runtime-main.de056b46.js"
+  },
+  {
+    "revision": "7c94be72174267b3aac61b9ab4df2af7",
+    "url": "/robots-react/static/media/BalooBhai2-Regular.7c94be72.ttf"
+  },
+  {
+    "revision": "59603ace30a3df9d19cb46b44615c2f7",
+    "url": "/robots-react/static/media/ROMEO.59603ace.TTF"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
